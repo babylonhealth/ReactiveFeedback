d6b16e5a4669513a6c3fe8999d149f416f36899f
sergdort <sergdort@gmail.com>
2017-09-04 22:05:22 +0100

    Add deferred operator to create signal every time we call start

--- ReactiveFeedback/Sources/SignalProducer+System.swift
+++ ReactiveFeedback/Sources/SignalProducer+System.swift
@@ -18,18 +18,16 @@ extension SignalProducerProtocol where Error == NoError {
     public static func system<Event>(initialState: Value,
                               reduce: @escaping Reducer<Value, Event>,
                               feedback: [FeedBack<Value, Event>]) -> SignalProducer<Value, NoError> {
-        
-        let (subject, observer) = Signal<Value, NoError>.pipe()
-
-        let events = Signal<Event, NoError>.merge(feedback.map { feedback in
-            return feedback(subject)
-        })
-        
-        let stateSignal = events.scan(initialState, reduce)
-        
-        return SignalProducer(stateSignal)
-            .prefix(value: initialState)
-            .on(value: observer.send(value:))
+        return SignalProducer
+            .deferred {
+                let (subject, observer) = Signal<Value, NoError>.pipe()
+                let events = Signal<Event, NoError>.merge(feedback.map { feedback in
+                    return feedback(subject)
+                })
+                return SignalProducer(events.scan(initialState, reduce))
+                    .prefix(value: initialState)
+                    .on(value: observer.send(value:))
+            }
     }
     
     public static func system<Event>(initialState: Value,
@@ -42,5 +40,12 @@ extension SignalProducerProtocol where Error == NoError {
                               reduce: @escaping Reducer<Value, Event>,
                               feedback: FeedBack<Value, Event>) -> SignalProducer<Value, Error> {
         return system(initialState: initialState, reduce: reduce, feedback: feedback)
+    }
+}
+
+extension SignalProducerProtocol {
+    static func deferred(_ signalProducerFactory: @escaping () -> SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
+        return SignalProducer<Void, Error>(value: ())
+            .flatMap(.merge, signalProducerFactory)
     }
 }
