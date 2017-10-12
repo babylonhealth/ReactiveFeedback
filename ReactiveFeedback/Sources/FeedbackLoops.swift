a8f409fa265d794401db7b7b655739c0effe7c19
sergdort <sergdort@gmail.com>
2017-10-12 22:59:42 +0100

    Make Feedback loop generic struct

--- ReactiveFeedback/Sources/FeedbackLoops.swift
+++ ReactiveFeedback/Sources/FeedbackLoops.swift
@@ -2,30 +2,38 @@ import Foundation
 import ReactiveSwift
 import enum Result.NoError
 
-public struct React<State:SchedulerProvidable, Event> {
+public struct FeedbackLoop<State, Event> {
+    let loop: (Signal<State, NoError>) -> Signal<Event, NoError>
+    
+    init(_ loop: @escaping (Signal<State, NoError>) -> Signal<Event, NoError>) {
+        self.loop = loop
+    }
+}
+
+extension FeedbackLoop where State: SchedulerProvidable {
     public static func feedback<Control:Equatable>(query: @escaping (State) -> Control?,
                                                    effects: @escaping (Control) -> Signal<Event, NoError>) -> FeedbackLoop<State, Event> {
-        return { state in
+        return FeedbackLoop { state in
             return state.map(query)
                 .skipRepeats { $0 == $1 }
                 .skipNil()
                 .flatMap(.latest, effects)
         }
     }
-
+    
     public static func feedback<Control:Equatable>(query: @escaping (State) -> Control?,
                                                    effects: @escaping (Control) -> SignalProducer<Event, NoError>) -> FeedbackLoop<State, Event> {
-        return { state in
+        return FeedbackLoop { state in
             return state.map(query)
                 .skipRepeats { $0 == $1 }
                 .skipNil()
                 .flatMap(.latest, effects)
         }
     }
-
+    
     public static func feedback(predicate: @escaping (State) -> Bool,
                                 effects: @escaping (State) -> Signal<Event, NoError>) -> FeedbackLoop<State, Event> {
-        return { state in
+        return FeedbackLoop { state in
             return state.filter(predicate)
                 .flatMap(.latest, { state in
                     return effects(state)
@@ -33,10 +41,10 @@ public struct React<State:SchedulerProvidable, Event> {
                 })
         }
     }
-
+    
     public static func feedback(predicate: @escaping (State) -> Bool,
                                 effects: @escaping (State) -> SignalProducer<Event, NoError>) -> FeedbackLoop<State, Event> {
-        return { state in
+        return FeedbackLoop { state in
             return state.filter(predicate)
                 .flatMap(.latest, { state in
                     return effects(state)
@@ -44,18 +52,18 @@ public struct React<State:SchedulerProvidable, Event> {
                 })
         }
     }
-
+    
     public static func feedback(effects: @escaping (State) -> Signal<Event, NoError>) -> FeedbackLoop<State, Event> {
-        return { state in
+        return FeedbackLoop { state in
             return state.flatMap(.latest) { state in
                 return effects(state)
                     .observe(on: state.scheduler)
             }
         }
     }
-
+    
     public static func feedback(effects: @escaping (State) -> SignalProducer<Event, NoError>) -> FeedbackLoop<State, Event> {
-        return { state in
+        return FeedbackLoop { state in
             return state.flatMap(.latest) { state in
                 return effects(state)
                     .enqueue(on: state.scheduler)
--- ReactiveFeedback/Sources/SignalProducer+System.swift
+++ ReactiveFeedback/Sources/SignalProducer+System.swift
@@ -2,26 +2,24 @@ import Foundation
 import ReactiveSwift
 import enum Result.NoError
 
-public typealias FeedbackLoop<State, Event> = (Signal<State, NoError>) -> Signal<Event, NoError>
 public typealias Reducer<State, Event> = (State, Event) -> State
 
 extension SignalProducerProtocol where Error == NoError {
-    
+
     public static func system<Event>(initialState: Value,
                                      reduce: @escaping Reducer<Value, Event>,
                                      feedback: [FeedbackLoop<Value, Event>]) -> SignalProducer<Value, NoError> {
-        return SignalProducer
-            .deferred {
-                let (subject, observer) = Signal<Value, NoError>.pipe()
-                let events = Signal<Event, NoError>.merge(feedback.map { feedback in
-                    return feedback(subject)
-                })
-                return SignalProducer(events.scan(initialState, reduce))
-                    .prefix(value: initialState)
-                    .on(value: observer.send(value:))
+        return SignalProducer.deferred {
+            let (subject, observer) = Signal<Value, NoError>.pipe()
+            let events = Signal<Event, NoError>.merge(feedback.map { feedback in
+                return feedback.loop(subject)
+            })
+            return SignalProducer(events.scan(initialState, reduce))
+                .prefix(value: initialState)
+                .on(value: observer.send(value:))
         }
     }
-    
+
     public static func system<Event>(initialState: Value,
                                      reduce: @escaping Reducer<Value, Event>,
                                      feedback: FeedbackLoop<Value, Event>...) -> SignalProducer<Value, Error> {
