dd5623378ca7ad44550cfe5fded607ab53d2188f
sergdort <sergdort@gmail.com>
2018-01-29 17:22:06 +0000

    Documentation to the Feedback (#12)
    * Feedback documented

--- ReactiveFeedback/Feedback.swift
+++ ReactiveFeedback/Feedback.swift
@@ -3,12 +3,22 @@ import ReactiveSwift
 import enum Result.NoError
 
 public struct Feedback<State, Event> {
-    public let events: (Scheduler, Signal<State, NoError>) -> Signal<Event, NoError>
+    let events: (Scheduler, Signal<State, NoError>) -> Signal<Event, NoError>
     
+    /// Creates an arbitrary Feedback, by transforming a sequence of State to a sequence of Events that mutate the State
+    ///
+    /// - parameters:
+    ///    - events: A closure which transforms the Signal<State, NoError> to the Signal<Event, NoError>
     public init(events: @escaping (Scheduler, Signal<State, NoError>) -> Signal<Event, NoError>) {
         self.events = events
     }
 
+    /// Creates a Feedback which will perform effects when query exists (i.e. is not nil)
+    /// and is different from the previous one, otherwise, it cancels any previously performed effects
+    ///
+    /// - parameters:
+    ///     - query: A closure which defines for which value perform the effect
+    ///     - effects: A sequence of the Events over time that mutate the State
     public init<Control: Equatable, Effect: SignalProducerConvertible>(
         query: @escaping (State) -> Control?,
         effects: @escaping (Control) -> Effect
@@ -25,6 +35,12 @@ public struct Feedback<State, Event> {
         }
     }
 
+    /// Creates a Feedback which will perform effects when `query` exists (i.e not nil)
+    /// otherwise, it cancels any previously performed effects
+    ///
+    /// - parameters:
+    ///    - query: A closure which defines for which value perform the effect
+    ///    - effects: A sequence of the Events over time that mutate the State
     public init<Control, Effect: SignalProducerConvertible>(
         query: @escaping (State) -> Control?,
         effects: @escaping (Control) -> Effect
@@ -40,20 +56,27 @@ public struct Feedback<State, Event> {
         }
     }
 
+    /// Creates a Feedback which will perform effects on for certain state filtered by the predicate.
+    /// Each new effect cancels the previous one
+    /// - parameters:
+    ///    - predicate: A closure which defines weather an effect should be performed to particular a value of the `State`
+    ///    - effects: A sequence of the Events over time that mutate the State
     public init<Effect: SignalProducerConvertible>(
         predicate: @escaping (State) -> Bool,
         effects: @escaping (State) -> Effect
     ) where Effect.Value == Event, Effect.Error == NoError {
         self.events = { scheduler, state in
-            return state
+            return state.filter(predicate)
                 .flatMap(.latest) { state -> SignalProducer<Event, NoError> in
-                    guard predicate(state) else { return .empty }
                     return effects(state).producer
                         .enqueue(on: scheduler)
             }
         }
     }
 
+    /// Creates a Feedback which will perform effects for each state change, canceling previously performed ones
+    /// - parameters:
+    ///    - effects: A sequence of the Events over time that mutate State
     public init<Effect: SignalProducerConvertible>(
         effects: @escaping (State) -> Effect
     ) where Effect.Value == Event, Effect.Error == NoError {
@@ -65,11 +88,3 @@ public struct Feedback<State, Event> {
         }
     }
 }
-
-fileprivate extension SignalProducer {
-    func enqueue(on scheduler: Scheduler) -> SignalProducer<Value, Error> {
-        return start(on: scheduler)
-            .observe(on: scheduler)
-    }
-}
-
--- ReactiveFeedback/SignalProducer+System.swift
+++ ReactiveFeedback/SignalProducer+System.swift
@@ -3,6 +3,17 @@ import ReactiveSwift
 import enum Result.NoError
 
 extension SignalProducer where Error == NoError {
+
+    /// Feedback-controlled State Machine. The system state is represented as a `State` parameter.
+    /// Events are represented by an `Event` parameter. It represents all the possible Events that may occur in the System
+    /// Feedback defines an effect that may change the State.
+
+    /// - parameters:
+    ///     - initial: An initial `State` of the system.
+    ///     - scheduler: A Scheduler used for Events synchronisation
+    ///     - reduce: A function that produces a new State of a system by applying an Event
+    ///     - feedbacks: A Feedback loops that produces Events depending on the system's `State`
+    ///     - returns: A SignalProducer that emits current the state of the System
     public static func system<Event>(
         initial: Value,
         scheduler: Scheduler = QueueScheduler.main,
@@ -23,8 +34,19 @@ extension SignalProducer where Error == NoError {
         }
     }
 
+    /// Feedback-controlled State Machine. The system state is represented as a `State` parameter.
+    /// Events are represented by `Event` parameter. It represents all possible Events that may occur in the System
+    /// Feedback defines an effect that may change the State.
+
+    /// - parameters:
+    ///     - initial: An initial `State` of the system.
+    ///     - scheduler: A Scheduler used for Events synchronisation
+    ///     - reduce: A that produces a new State of the system by applying an Event
+    ///     - feedbacks: A Feedback loops that produces Events depending on the system's state.
+    ///     - returns: A SignalProducer that emits current the state of the System
     public static func system<Event>(
         initial: Value,
+        scheduler: Scheduler = QueueScheduler.main,
         reduce: @escaping (Value, Event) -> Value,
         feedbacks: Feedback<Value, Event>...
     ) -> SignalProducer<Value, Error> {
@@ -33,5 +55,10 @@ extension SignalProducer where Error == NoError {
 
     private static func deferred(_ producer: @escaping () -> SignalProducer<Value, Error>) -> SignalProducer<Value, Error> {
         return SignalProducer { $1 += producer().start($0) }
+    }
+
+    func enqueue(on scheduler: Scheduler) -> SignalProducer<Value, Error> {
+        return producer.start(on: scheduler)
+            .observe(on: scheduler)
     }
 }
