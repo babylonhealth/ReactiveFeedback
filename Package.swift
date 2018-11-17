ae89841ca83a5af2c3f77337af4c93bca563040e
David Rodrigues <david.ndh@gmail.com>
2018-11-17 14:06:17 +0000

    Add support for Swift Package Manager (#34)
    * Add support for Swift Package Manager
    
    * Update CI to test Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "ReactiveFeedback",
+    products: [
+        .library(name: "ReactiveFeedback", targets: ["ReactiveFeedback"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", from: "4.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.0"),
+    ],
+    targets: [
+        .target(name: "ReactiveFeedback", dependencies: ["ReactiveSwift"], path: "ReactiveFeedback"),
+        .testTarget(name: "ReactiveFeedbackTests", dependencies: ["ReactiveFeedback", "ReactiveSwift", "Nimble"], path: "ReactiveFeedbackTests"),
+    ],
+    swiftLanguageVersions: [4]
+)
